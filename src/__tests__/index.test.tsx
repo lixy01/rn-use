87873c2051fada7838cb9d427c5ee60956d43bf1
lixy <540755229@qq.com>
2023-02-28 13:46:01 +0800

    doc

--- src/__tests__/index.test.tsx
+++ src/__tests__/index.test.tsx
@@ -1,1 +1,8 @@
-it.todo('write a test');
+import { multiply } from 'rn-use';
+
+it.todo('index.jest');
+test('multiply', () => {
+  return multiply(1, 2).then((res) => {
+    expect(res).toBe(2);
+  });
+});
